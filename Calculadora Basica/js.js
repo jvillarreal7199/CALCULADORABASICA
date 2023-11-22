cb207bbb8d9e16e175143683abe32a603b83262b
jvillarreal7199 <149267883+jvillarreal7199@users.noreply.github.com>
2023-11-21 21:44:43 -0500

    Add files via upload

---
+++ Calculadora Basica/js.js
@@ -0,0 +1,36 @@
+
+
+function sumar (){
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 + numero2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function restar (){
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 - numero2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function multiplicar (){
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 * numero2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function dividir (){
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var resultado = numero1 / numero2;
+    document.getElementById("resultado").value = resultado;
+}
+
+function limpiar (){
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").value = "";
+}
+
